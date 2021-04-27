2a544ee0b55ec7ab6ac2ab8dbbf33676484af627
Luis Adrian Arellano Ruiz <luis.arellano@alignet.com>
2021-04-27 00:01:28 -0500

    test

--- db/get_collection.py
+++
@@ -1,9 +0,0 @@
-import traceback
-import pymongo as py
-from get_connection import mongodb_conn
-def mongodb_collection(conn,dbname,collection):
-    try:
-        return conn.dbname.collection
-    except py.errors.CollectionInvalid as e:
-        traceback.print_exc()
-        print("No se encontró la colección en la base de datos: %s" %e)
